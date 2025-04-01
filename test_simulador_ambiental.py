a0335a8c8054d7da12587c0ab0716c194e0d70ec
ESTEFANY HERNÁNDEZ LIZCANO <161661453+FANYTTK@users.noreply.github.com>
2025-04-01 11:57:52 -0600

    Update test_simulador_ambiental.py

--- test_simulador_ambiental.py
+++ test_simulador_ambiental.py
@@ -1,1 +1,21 @@
+from simulador_ambiental import SimuladorAmbiental
+
+sim = SimuladorAmbiental(
+    ancho=10,
+    alto=10,
+    temperatura=35,
+    humedad=20,
+    viento=5,
+    direccion_viento='E',
+    oxigenacion=22,
+    combustible=0.8
+)
+
+sim.encender(5, 5)
+
+while not sim.terminado():
+    sim.mostrar()
+    sim.paso()
+
+sim.mostrar()
 
